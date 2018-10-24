7c7e786a52f8e70cbf5660d9aad40b065cec5eaa
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-24 21:55:14 +0200

    add the src and play the object

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-var jukebox = document.querySelector("ul.player");
-jukebox.addEventListener("click", function(e) {
-    var audioPlayer = document.createElement("audio");
-    audioPlayer.id = "player";
-    document.body.appendChild(audioPlayer);
+var jukebox = document.querySelector("ul.player"); // grab the jukebox ul
+jukebox.addEventListener("click", function(e) { // add event handler for thr jukebox ul
+    var audioPlayer = document.createElement("audio"), // creating an media object
+        songName = e.target.getAttribute("data-src"); // git the clicked target data src attribute
+    audioPlayer.id = "player"; // set an id for the object
+    audioPlayer.src = songName; // set the src for the object
+    document.body.appendChild(audioPlayer); // append the object to the body
+    audioPlayer.play(); // play the object
 }, false);
