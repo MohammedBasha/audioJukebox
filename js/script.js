61d0ad24973fd29aa3b07635d390d3ebf8583513
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-25 00:36:23 +0200

    add event handler and styling for the ended song

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@ jukebox.addEventListener("click", function(e) { // add event handler for thr juk
     var audioPlayer = document.createElement("audio"), // creating an media object
         songName = e.target.getAttribute("data-src"); // git the clicked target data src attribute
     audioPlayer.id = "player"; // set an id for the object
+    e.target.id = "playing"; // adding the predefined id styling to the targeted song
     audioPlayer.src = songName; // set the src for the object
     document.body.appendChild(audioPlayer); // append the object to the body
     audioPlayer.play(); // play the object
+
+    audioPlayer.addEventListener("ended", function() { // adding an event handler for the ended song
+        audioPlayer.parentNode.removeChild(audioPlayer); // remove the audio object
+        e.target.id = ""; // reset the id for nothing
+    }, false);
 }, false);
