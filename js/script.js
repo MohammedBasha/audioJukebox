4f2e8aad0b9eb65684fd30301f213b17cf55dcea
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-25 00:52:46 +0200

    addinig a more control checking for the played song

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 var jukebox = document.querySelector("ul.player"); // grab the jukebox ul
 jukebox.addEventListener("click", function(e) { // add event handler for thr jukebox ul
-    var audioPlayer = document.createElement("audio"), // creating an media object
-        songName = e.target.getAttribute("data-src"); // git the clicked target data src attribute
-    audioPlayer.id = "player"; // set an id for the object
-    e.target.id = "playing"; // adding the predefined id styling to the targeted song
-    audioPlayer.src = songName; // set the src for the object
-    document.body.appendChild(audioPlayer); // append the object to the body
-    audioPlayer.play(); // play the object
+    var songName = e.target.getAttribute("data-src"), // get the clicked target data src attribute
+        songPlaying = document.querySelector("#player"); // get the played song
 
-    audioPlayer.addEventListener("ended", function() { // adding an event handler for the ended song
-        audioPlayer.parentNode.removeChild(audioPlayer); // remove the audio object
-        e.target.id = ""; // reset the id for nothing
-    }, false);
+    if (songPlaying) { // check if the song is playing
+        if (songPlaying.paused) { // check if the song is paused
+            songPlaying.play(); // play it
+            e.target.id = "playing"; // add the proper id for it
+        } else { // if not paused
+            songPlaying.pause(); // pause it
+            e.target.id = "paused"; // add the prper id for it
+        }
+    } else { // if not playing
+        var audioPlayer = document.createElement("audio"); // creating an media object
+        audioPlayer.id = "player"; // set an id for the object
+        e.target.id = "playing"; // adding the predefined id styling to the targeted song
+        audioPlayer.src = songName; // set the src for the object
+        document.body.appendChild(audioPlayer); // append the object to the body
+        audioPlayer.play(); // play the object
+
+        audioPlayer.addEventListener("ended", function() { // adding an event handler for the ended song
+            audioPlayer.parentNode.removeChild(audioPlayer); // remove the audio object
+            e.target.id = ""; // reset the id for nothing
+        }, false);
+    }
+    
 }, false);
